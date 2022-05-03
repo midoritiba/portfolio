b134171fe04ece5730216d00cf4132cacdebf6cc
midoritiba <mic.tiba@gmail.com>
2022-05-03 11:52:33 +0200

    scrollup function

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,40 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Homepage from './pages/Homepage';
+import { FaArrowAltCircleUp } from "react-icons/fa";
+import {HashLink as Link} from 'react-router-hash-link'
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  const [showButton, setShowButton] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    });
+  }, []);
+
   return (
     <Router>
+      <Navbar />
       <Homepage/>
+      {showButton && (
+            <Link to="#home" >
+              <FaArrowAltCircleUp
+              size={42} 
+              style={
+                {position: 'fixed', 
+                bottom:'5%', 
+                right: '5%', 
+                cursor: 'pointer',
+                color: '#FF5CED'}
+              }/>
+            </Link>)
+          }
     </Router>
   );
 }
