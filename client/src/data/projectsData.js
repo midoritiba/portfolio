4b6d17623e183ac61ae6e4fcea5bd478de76a01e
midoritiba <mic.tiba@gmail.com>
2022-05-03 13:50:17 +0200

    projects data

---
+++ client/src/data/projectsData.js
@@ -0,0 +1,23 @@
+export const projectsData = [
+    {
+      id: 1,
+      title: 'Coffee Shop | Change and Tip Calculator',
+      technologies: ['HTML', 'CSS', 'Javascript', 'Bootstrap', 'Netlify'],
+      description: ' A responsive web application for employees to use it as a bill calculator. The cashier can view all items of the menu (image, category, name and price), add items to cart, increase, decrease or remove units of each item added. The app will show the total items in the cart, the subtotal price, the value of a 10% tip, the total price, and corresponding change if necessary. After registering the purchase, we can move on to the next customer. <br/>For this project, the following concepts were applied:',
+      features: ['Classes Constructors', 'DOM Manipulation', 'Array.forEach()', 'onClick Event'],
+      demo: 'https://change-tip-calculator.netlify.app/',
+      code: 'https://github.com/midoritiba/CoffeShop-Cash-register-Project',
+      image: '/images/project1.svg',
+    },
+    // {
+    //     id: 2,
+    //     title: '',
+    //     technologies: [],
+    //     description: '',
+    //     features: [],
+    //     demo: '',
+    //     code: '',
+    //     image: '',
+    // }
+
+  ];
