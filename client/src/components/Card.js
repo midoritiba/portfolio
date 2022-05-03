5235ebf0bcc3b049eac2951ca62ec634d933f61f
midoritiba <mic.tiba@gmail.com>
2022-05-03 13:45:08 +0200

    project-card component

---
+++ client/src/components/Card.js
@@ -0,0 +1,50 @@
+const Card = ({id, title, technologies, description, features, demo, code, image}) => {
+  return (
+    <div className="card" id={id}>
+
+    <div className="details">
+      <h2>{title}</h2>
+        <div className="technologies">
+            {technologies.map(technology => (
+                <div className="technologie-wrapper mt-3 me-3" id={technology}>
+                    {technology}
+                </div>
+            ))}
+        </div>
+
+      <div className="project-description pt-3 pe-3">
+        {description}
+        <ul className='mt-2'>
+            {features.map(feature => (
+                <li>{feature}</li>
+            ))}
+        </ul>
+      </div>
+
+      <div className="project-buttons mt-2">
+        <a rel='noopener noreferrer' target='_blank' className='link text-decoration-none' href={demo}>
+          <div className="btn fill">
+           demo
+          </div>
+        </a>
+
+        <a rel='noopener noreferrer' target='_blank' className='link text-decoration-none' href={code}>
+            <div className="btn fill ms-3">
+            code
+            </div>
+        </a>
+
+      </div>
+    </div>
+
+    <div className="project-mockup">
+    <img 
+        src={process.env.PUBLIC_URL + image} 
+        style={{width: '100%', objectFit: 'cover', objectPosition: 'center', borderRadius: '20px'}} 
+        alt={title} />
+    </div>
+  </div>
+  )
+}
+
+export default Card
