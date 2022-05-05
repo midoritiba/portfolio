3726322ba8970fe5afe88737f5f6aaa8172fba49
midoritiba <mic.tiba@gmail.com>
2022-05-05 21:41:54 +0200

    contact minor error

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -12,12 +12,10 @@ const Contact = () => {
       <h1 className='mb-3'>Get in touch</h1>
       <h5 className="mt-1 mb-5">Don't hesitate contacting me 🤗</h5>
       <div className="contact-wrapper">
-          <ContactForm />    
-
-          
-        <div className='d-flex flex-column text-center form-img'>
-          <img  src={process.env.PUBLIC_URL + '/images/contact.png'} style={{width: '300px', borderRadius: '50%'}} alt="" /> 
-        </div>   
+          <ContactForm />            
+          <div className='d-flex flex-column text-center form-img'>
+            <img  src={process.env.PUBLIC_URL + '/images/contact.png'} style={{width: '300px', borderRadius: '50%'}} alt="" /> 
+          </div>   
       </div>
      
 
